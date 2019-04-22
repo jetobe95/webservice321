584c0040e862fe53a85b5f8d55cfca8800508efa
Jeffrey <jtorres@technisupport.com>
2019-04-22 09:48:53 -0500

    HBS

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const multiparty = require('connect-multiparty');
+const exphbs = require("express-handlebars");
+
+
 const path = require('path')
 const app = express();
 
 const imagesDirectory = path.join(__dirname, 'public/images')
 app.set('PORT', process.env.PORT || 3000)
 app.use(express.static(imagesDirectory))
+app.set('views',path.join(__dirname,'src','views'))
 
+app.engine('.hbs',exphbs({
+    defaultLayout: 'main',
+    layoutsDir:path.join(app.get('views'),'layouts'),
+    partialsDir:path.join(app.get('views'),'partials'),
+    extname:'.hbs',
+    // helpers:require("./lib/handlerbars")
+}))
+
+app.set('view engine','.hbs')
 
 const multipartyMiddleware = multiparty({ uploadDir: imagesDirectory, });
-
+const LINKS = [];
 
 
 app.post('/upload', multipartyMiddleware, (req, res) => {
-    
+    LINKS.push(req.files.file.originalFilename)
+    console.log('files',LINKS);
     res.json({
         success:true,
         message:'Archivos recibidos'
     })
 })
 app.get("/",(req,res)=>{
-    res.send('WebService')
+    res.render('listado',{links:LINKS})
 })
 
 
